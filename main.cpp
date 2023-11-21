4af7445591cc3b4989e247f21b1ea882c287cb39
Rahma, A. Z. R <118007550+alyazahrarr@users.noreply.github.com>
2023-11-21 23:02:53 +0700

    progress tugas day 2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,80 @@
 # include <iostream>
+# include <vector>
+using namespace std;
+
+void lokasi(int n, vector<int>& a, vector<int>& b){
+    // print elemen di langkah ke-n
+    cout << "(" << a[n] << "," << b[n] << ")" << endl;
+}
+
+void gerak(int n, int x, int y, vector<int>& a, vector<int>& b){
+    // membersihkan elemen
+    if (a.size() - 1 > n){
+        for (int i = n; i < a.size(); i++){
+            a.pop_back();
+            b.pop_back();}
+    }
+    // elemen terakhir ditambah x dan y
+    a.push_back(a[a.size() - 1] + x);
+    b.push_back(b[b.size() - 1] + y);
+}
 
 int main(){
+    // opening
+    char opsi {};
+    cout << "Selamat Datang di Simulasi Pergerakan Drone!" << '\n' << "============================================" << endl;
+    cout << "1   Pergerakan Drone dengan Koordinat" << '\n' << "2   Pergerakan Drone dengan Kecepatan dan Waktu" << '\n' << "Pilih opsi: ";
+    cin >> opsi;
+    while (!(opsi == '1') || !(opsi != '2')){
+        cout << "Opsi tidak tersedia. Pilih opsi: ";
+        cin >> opsi;}
+
+    // Program Pergerakan Drone dengan Koordinat
+    if (opsi == '1'){
+        int langkah {0};
+        // menyimpan histori lokasi
+        vector<int> historix {0};
+        vector<int> historiy {0};
+        // menyimpan input
+        int x, y;
+        char ingin {};
+
+        cout << "============================================" << '\n' << "Pergerakan Drone dengan Koordinat" << '\n';
+        cout << "1   Pergerakan" << '\n' << "2   Undo" << '\n' << "3   Redo" << '\n' << "4   Cek Lokasi" << '\n' << "5   Keluar" << endl;
+        cout << "Ingin melakukan apa? ";
+        cin >> ingin;
+        // selama belum pilih opsi exit
+        while (!(ingin == '5')){
+            if (ingin == '1'){ // pergerakan
+                cout << "Gerakan drone (dalam koordinat kartesius)? ";
+                cin >> x >> y;
+                gerak(langkah, x, y, historix, historiy);
+                langkah = langkah + 1;}
+            if (ingin == '2'){ // undo
+                if (!(langkah == 0)){
+                langkah = langkah - 1;
+                cout << "Undo berhasil!" << endl;}
+                else 
+                cout << "Undo tidak bisa dilakukan..." << endl;}
+            if (ingin == '3'){ // redo
+                if (!(langkah == historix.size() - 1)){
+                langkah = langkah + 1;
+                cout << "Redo berhasil!" << endl;}
+                else 
+                cout << "Redo tidak bisa dilakukan..." << endl;}
+            if (ingin == '4'){ // cek lokasi
+                cout << "Lokasi saat ini: ";
+                lokasi(langkah, historix, historiy);}    
+            cout << "Ingin melakukan apa? ";
+            cin >> ingin;
+            }}
+
+    // Program Pergerakan Drone dengan Kecepatan dan Waktu
+    if (opsi == '2'){
+        cout << "opsi 2";
+    }
+
+    // closing
+    cout << "Terima kasih telah menggunakan simulasi ini!";
     return 0;
 }
